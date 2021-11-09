c78bd84d08259028a484b0f6281f482a699d4546
renanfortkamp <renan_fortkamp@estudante.sesisenai.org.br>
2021-11-08 23:56:38 -0300

    Implementando função para adicionar tarefas ao array e implementado função com repetição para adicionar , excluir e atualizar lista na pagina.

--- scripts/script.js
+++ scripts/script.js
@@ -1,21 +1,35 @@
 
 const todoList = [];
 
-task.addEventListener('click', function(){
+addTask.addEventListener('click', function(){
+  let task = document.querySelector('input').value;
+  if(task == ""){
+    alert("Insira uma tarefa!")
+  } else{
     let task = document.querySelector('input').value;
     todoList.push(task);
     createTask();
-} 
-
+   }
+  }
 )
+
 const createTask = ()=>{
-  document.querySelector('ul').innerHTML = ""  
-  for(i=0 ; i < todoList.length; i++){
-    let addTolist = document.querySelector('ul');
-    let li = document.createElement('li');
-    let taskValue = document.createTextNode(todoList[i]);
-    li.appendChild(taskValue);
-    addTolist.appendChild(li);    
+    document.querySelector('ul').innerHTML = "";
     document.querySelector('input').value = "";
+  for(let i=0 ; i < todoList.length; i++){
+    let addTolist = document.querySelector('ul');
+    let task = document.createTextNode(todoList[i]);
+    let li = document.createElement('li');
+    li.appendChild(task);
+    addTolist.appendChild(li);
+    let deleteTask = document.createElement('span');
+    let x = document.createTextNode("X");
+    deleteTask.appendChild(x);
+    li.appendChild(deleteTask);
+    deleteTask.addEventListener('click', ()=>{
+        todoList.splice(i,1);
+        createTask();
+      } 
+    )
+  }
 }  
-}
